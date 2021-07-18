b9c30b9d358f931b7f4862178a16094a2f6cd99f
anandbaraik <anandbaraik2014@gmail.com>
2021-07-18 22:19:59 +0530

    Header component added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <div className="header-container">
+        <img className="header-img" src="https://user-images.githubusercontent.com/26179770/106359099-693e3380-6336-11eb-8069-9e36f25de5ca.png" alt="nameit-logo"></img>
+            <h1 className="header-text">Name It!</h1>
+        </div>
+    );
+}
+
+export default Header;
